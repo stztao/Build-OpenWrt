b06c6dafd1e34c06c982f5228866d899585d793a
stztao <57539266+stztao@users.noreply.github.com>
2021-11-17 03:58:35 +0800

    Update diy-2.sh

--- build/openwrt/diy-2.sh
+++ build/openwrt/diy-2.sh
@@ -12,4 +12,4 @@
 #
 # 修改网关（把192.168.2.2改成你需要的就行）
 #sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
