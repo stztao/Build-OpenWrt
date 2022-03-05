502a5a7dc2bbf1d1edffa7f927d3d006f42f46c9
stztao <57539266+stztao@users.noreply.github.com>
2022-03-06 07:56:08 +0800

    Update diy-1.sh

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -13,6 +13,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # lede源码增加passwall,也适用于增加其他插件的方法
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-sed -i '$a src-git packages https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
